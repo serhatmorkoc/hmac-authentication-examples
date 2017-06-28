2659110ecd093dd95273ce372d364c7325d93edb
serhat morkoç <serhatmorkoc@gmail.com>
2017-06-28 23:53:45 +0300

    -comm

--- src/HMACAuthentication.Client/API.cs
+++ src/HMACAuthentication.Client/API.cs
@@ -12,10 +12,10 @@ namespace HMACAuthentication.Client
         {
             public static string getProducts(string apiUrl, string apiUser, string apiKey)
             {
-                //&Signature={5}
+                var datetime = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss+03:00", new System.Globalization.CultureInfo("tr-TR"));
 
-                var baseFormat = "Action={0}&Timestamp={1}&UserID={2}&Version={3}&Format={4}";
-                var baseString = string.Format(baseFormat, WebUtility.UrlEncode("GetProducts"), WebUtility.UrlEncode(DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ss-0000")), WebUtility.UrlEncode("maintenance@sellercenter.net"), "1.0", "XML");
+                var baseFormat = "Action={0}&Filter=all&Format=XML&Timestamp={1}&UserID={2}&Version=1.0";
+                var baseString = string.Format(baseFormat, WebUtility.UrlEncode("GetProducts"), WebUtility.UrlEncode(datetime), WebUtility.UrlEncode(apiUser));
                 var oauth_signature = CreateSignature(apiKey, baseString);
                 return apiUrl + baseString + "&Signature=" +  oauth_signature;
             }
@@ -34,17 +34,15 @@ namespace HMACAuthentication.Client
 
         }
 
-        private static string CreateSignature(string apiKey, string message )
+        private static string CreateSignature( string apiKey, string message)
         {
-            var encoding = new ASCIIEncoding();
-            var keyByte = encoding.GetBytes(apiKey);
-            var messageBytes = encoding.GetBytes(message);
-            using (var hmacsha256 = new HMACSHA256(keyByte))
-            {
-                var hashmessage = hmacsha256.ComputeHash(messageBytes);
-                var signature = Convert.ToBase64String(hashmessage);
-                return WebUtility.UrlEncode(signature);
-            }
+            var key1 = Encoding.UTF8.GetBytes(apiKey);
+            var message1 = Encoding.UTF8.GetBytes(message);
+            var hmacSHA = new HMACSHA256(key1);
+            var hash = hmacSHA.ComputeHash(message1, 0, message1.Length);
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+
+ 
         }
     }
 }
--- src/HMACAuthentication.Client/Program.cs
+++ src/HMACAuthentication.Client/Program.cs
@@ -10,9 +10,9 @@ namespace HMACAuthentication.Client
 {
     class Program
     {
-        static readonly string _apiUrl = "https://api.sellercenter.lazada.sg/?";
-        static readonly string _apiUser = "api@sellercenter.net";
-        static readonly string _apiKey = "b1bdb357ced10fe4e9a69840cdd4f0e9c03d77fe";
+        static readonly string _apiUrl = "https://api.sellercenter.lamoda.ua/?";
+        static readonly string _apiUser = "globalmarkets+marketplacetest@b2cdirect.com";
+        static readonly string _apiKey = "5a8cdd2527e4e682fa75434f449dac1600c098b6";
 
         static void Main(string[] args)
         {
